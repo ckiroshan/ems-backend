70f6b670b9575b8e81b30af790996bc011b12572
ckiroshan <ckiroshan@gmail.com>
2024-06-18 00:37:09 +0530

    feat: Implement getEmployeeById method in EmployeeServiceImpl

--- src/main/java/com/projects/ems/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/projects/ems/service/impl/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.projects.ems.service.impl;
 
 import com.projects.ems.dto.EmployeeDto;
 import com.projects.ems.entity.Employee;
+import com.projects.ems.exception.ResourceNotFoundException;
 import com.projects.ems.mapper.EmployeeMapper;
 import com.projects.ems.repository.EmployeeRepository;
 import com.projects.ems.service.EmployeeService;
@@ -21,5 +22,14 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee savedEmployee = employeeRepository.save(employee);
         // Convert Employee entity to EmployeeDto
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Employee with ID " + employeeId + " does not exist"));
+        // Convert Employee entity to EmployeeDto
+        return EmployeeMapper.mapToEmployeeDto(employee);
     }
 }
