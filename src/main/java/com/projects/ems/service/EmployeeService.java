4cfbadea7f3848c240783649103113fd89fbf712
ckiroshan <ckiroshan@gmail.com>
2024-06-19 01:11:30 +0530

    feat: Add getAllEmployees method to EmployeeService

--- src/main/java/com/projects/ems/service/EmployeeService.java
+++ src/main/java/com/projects/ems/service/EmployeeService.java
@@ -2,8 +2,12 @@ package com.projects.ems.service;
 
 import com.projects.ems.dto.EmployeeDto;
 
+import java.util.List;
+
 public interface EmployeeService {
     EmployeeDto addEmployee(EmployeeDto employeeDto);
 
     EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
 }
