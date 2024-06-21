4abd60f92f33c08a2e4c7c6d7fc8333c6c722192
ckiroshan <ckiroshan@gmail.com>
2024-06-22 01:41:03 +0530

    feat: Add updateEmployee endpoint in EmployeeController

--- src/main/java/com/projects/ems/controller/EmployeeController.java
+++ src/main/java/com/projects/ems/controller/EmployeeController.java
@@ -37,4 +37,12 @@ public class EmployeeController {
         return ResponseEntity.ok(employees);
     }
 
+    // Update existing employee
+    @PutMapping("{id}")
+    public ResponseEntity<EmployeeDto> updatedEmployee(@PathVariable("id") Long employeeId,
+                                                       @RequestBody EmployeeDto updatedEmployee) {
+        EmployeeDto employeeDto = employeeService.updateEmployee(employeeId, updatedEmployee);
+        return ResponseEntity.ok(employeeDto);
+    }
+
 }
