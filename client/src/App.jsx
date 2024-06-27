aadaf9ecc93a58f1d888c61514f21518cb9c9fe0
ckiroshan <ckiroshan@gmail.com>
2024-06-28 01:14:46 +0530

    feat: add FooterComponent with copyrights

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import FooterComponent from "./components/FooterComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <HeaderComponent />
       <ListEmployeeComponent />
+      <FooterComponent />
     </>
   );
 }
---
+++ client/src/components/FooterComponent.jsx
@@ -0,0 +1,11 @@
+const FooterComponent = () => {
+  return (
+    <div>
+      <footer className="footer">
+        <p>All rights reserved 2024 by ckiroshan</p>
+      </footer>
+    </div>
+  );
+};
+
+export default FooterComponent;
