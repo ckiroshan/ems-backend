92819b4cbbdccfbd3570eac978d50f7543086526
ckiroshan <ckiroshan@gmail.com>
2024-07-03 00:53:56 +0530

    feat: add conditional for add/update employee forms with dynamic title

--- client/src/components/EmployeeComponent.jsx
+++ client/src/components/EmployeeComponent.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import { createEmployee } from "../services/EmployeeService";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const EmployeeComponent = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
 
+  const { id } = useParams();
   const [errors, setErrors] = useState({
     firstName: "",
     lastName: "",
@@ -59,11 +60,19 @@ const EmployeeComponent = () => {
     return valid;
   }
 
+  function pageTitle() {
+    if (id) {
+      return <h2 className="text-center pt-4">Update Employee</h2>;
+    } else {
+      return <h2 className="text-center pt-4">Add Employee</h2>;
+    }
+  }
+
   return (
     <div className="container">
       <div className="row mt-5">
         <div className="card col-md-6 offset-md-3 offset-md-3 mt-4">
-          <h2 className="text-center pt-4">Add Employee</h2>
+          {pageTitle()}
           <div className="card-body">
             <form>
               <div className="form-group mb-2">
