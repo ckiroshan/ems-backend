46b0e56a00b8ebce76b406b30cbf6e5bace994e1
ckiroshan <ckiroshan@gmail.com>
2024-07-04 01:08:03 +0530

    feat: add functionality to fetch employee details based on ID

--- client/src/components/EmployeeComponent.jsx
+++ client/src/components/EmployeeComponent.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { createEmployee } from "../services/EmployeeService";
+import { useEffect, useState } from "react";
+import { createEmployee, getEmployee } from "../services/EmployeeService";
 import { useNavigate, useParams } from "react-router-dom";
 
 const EmployeeComponent = () => {
@@ -15,6 +15,20 @@ const EmployeeComponent = () => {
   });
 
   const navigator = useNavigate();
+
+  useEffect(() => {
+    if (id) {
+      getEmployee(id)
+        .then((response) => {
+          setFirstName(response.data.firstName);
+          setLastName(response.data.lastName);
+          setEmail(response.data.email);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }, [id]);
 
   function saveEmployee(e) {
     e.preventDefault();
